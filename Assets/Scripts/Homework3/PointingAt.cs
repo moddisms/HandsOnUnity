4ece60a86fd742ffa576c8ae91c34f36a3e61474
moddisms <doddmichaela96@gmail.com>
2024-10-14 15:58:10 +1300

    raycast work part 2

--- Assets/Scripts/Homework3/PointingAt.cs
+++ Assets/Scripts/Homework3/PointingAt.cs
@@ -6,24 +6,52 @@ public class PointingAt : MonoBehaviour
 {
 
     public Renderer ren;
+    [SerializeField] LineRenderer lineRenderer;
+
+
     // Start is called before the first frame update
     void FixedUpdate()
     {
         Vector3 fwd = transform.TransformDirection(Vector3.forward);
 
-        if (Physics.Raycast(transform.position, fwd, 10))
+        Debug.DrawRay(transform.position, fwd * 10, Color.green, 2);
+
+        RaycastHit a;
+
+
+        if (Physics.Raycast(transform.position, fwd, out a, 10))
+        {
             print("There is something in front of the object!");
-        ChangeColour();
+
+
+
+            ChangeColour(a.transform);
+            ////
+            //stuff to set positions on the line renderer
+/*
+
+            Vector3[] vectorArray = new Vector3[] { transform.position, a.point };
+            lineRenderer.SetPositions(vectorArray);
+*/
+
+        }
+
+        
     }
 
- void ChangeColour()
-    {      
-        ren = GetComponent<Renderer>();
+    public void ChangeColour(Transform a)
+    {
+        
+        ren = a.GetComponent<Renderer>();
 
-        ren.material.color = Color.white;    
+        ren.material.color = Color.white;
     }
 
 }
+
+
+
+
 
 
 
